1155690554642605b424d6870776360ed0459aba
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-10 16:55:28 -0500

    Removing integration tests from Test::Unit

--- lib/rails_blog/engine.rb
+++ lib/rails_blog/engine.rb
@@ -5,6 +5,7 @@ module RailsBlog
     config.generators do |g|
       g.template_engine :haml
       g.test_framework :rspec, :fixture => false
+      g.integration_tool :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
