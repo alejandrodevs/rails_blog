36ed163e8b40bed7d2384832b15f8b02c5b091f3
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-21 22:19:06 -0500

    Search listing posts view and adding post description method

--- app/models/rails_blog/post.rb
+++ app/models/rails_blog/post.rb
@@ -1,5 +1,9 @@
 module RailsBlog
   class Post < ActiveRecord::Base
     default_scope -> { order("created_at DESC") }
+
+    def description
+      self.content.first(200).chomp(" ") + "..."
+    end
   end
 end
--- spec/models/rails_blog/post_spec.rb
+++ spec/models/rails_blog/post_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 module RailsBlog
   describe Post do
-    pending "add some examples to (or delete) #{__FILE__}"
+    describe "#description" do
+      it "returns a two-hundred-long description" do
+        subject.content = "a" * 500
+        expect(subject.description).to eql(("a" * 200) + "...")
+      end
+    end
   end
 end
