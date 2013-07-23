07e501f65cf695a43ebb98e2b9107ef3f4cb5936
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-23 16:13:27 -0500

    Including rails_blog initializer file in Dummy project

---
+++ spec/dummy/config/initializers/rails_blog.rb
@@ -0,0 +1,17 @@
+RailsBlog.setup do |config|
+
+  # == Blog Title
+  #
+  # Set the title that is displayed on the main layout
+  # for each of the rails blog pages.
+  #
+  config.blog_title = "RailsBlog"
+
+  # == Small Blog Title
+  #
+  # Set the small title that is displayed beside the
+  # main title.
+  #
+  config.small_blog_title = "Blog"
+
+end
