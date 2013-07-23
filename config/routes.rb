308e4f1173ba83a107ae216ac8b0b89809626479
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-23 11:55:19 -0500

    Accepting only index and show post action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ RailsBlog::Engine.routes.draw do
   match "archive" => "archives#index", via: :get, as: "archive"
   match "search"  => "searches#index", via: :get, as: "search_posts"
 
-  resources :posts
+  resources :posts, only: [:index, :show]
 
   root "posts#index"
 
