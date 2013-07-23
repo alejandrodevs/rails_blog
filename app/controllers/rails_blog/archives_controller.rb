eaf32728c0d1cc4ac82ae2f59c5aeee52dee651d
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-23 11:16:21 -0500

    Archive view, Post grouped for archive and Sunspot commit in callback

--- app/controllers/rails_blog/archives_controller.rb
+++ app/controllers/rails_blog/archives_controller.rb
@@ -3,6 +3,7 @@ require_dependency "rails_blog/application_controller"
 module RailsBlog
   class ArchivesController < ApplicationController
     def index
+      @posts = Post.grouped_for_archive
     end
   end
 end
--- app/models/rails_blog/post.rb
+++ app/models/rails_blog/post.rb
@@ -1,13 +1,27 @@
 module RailsBlog
   class Post < ActiveRecord::Base
-    default_scope -> { order("created_at DESC") }
 
     searchable do
       text :title, :content
     end
 
+    after_save :update_sunspot
+
+    default_scope -> { order("created_at DESC") }
+
+    def update_sunspot
+      Sunspot.commit
+    end
+
     def description
       self.content.first(200).chomp(" ") + "..."
+    end
+
+    def self.grouped_for_archive
+      grouped_by_year = self.all.group_by{ |post| post.created_at.year }
+      grouped_by_year.each do |year, posts|
+        grouped_by_year[year] = posts.group_by{ |post| post.created_at.month }
+      end
     end
   end
 end
