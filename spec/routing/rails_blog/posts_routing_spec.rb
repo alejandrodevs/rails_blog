1e0af729e16139c3e0e67d7f48d1356a03af392f
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-24 11:17:58 -0500

    Removing unnecessary post views and tests

--- spec/routing/rails_blog/posts_routing_spec.rb
+++ spec/routing/rails_blog/posts_routing_spec.rb
@@ -8,28 +8,8 @@ module RailsBlog
         get("/posts").should route_to("posts#index")
       end
 
-      it "routes to #new" do
-        get("/posts/new").should route_to("posts#new")
-      end
-
       it "routes to #show" do
         get("/posts/1").should route_to("posts#show", :id => "1")
-      end
-
-      it "routes to #edit" do
-        get("/posts/1/edit").should route_to("posts#edit", :id => "1")
-      end
-
-      it "routes to #create" do
-        post("/posts").should route_to("posts#create")
-      end
-
-      it "routes to #update" do
-        put("/posts/1").should route_to("posts#update", :id => "1")
-      end
-
-      it "routes to #destroy" do
-        delete("/posts/1").should route_to("posts#destroy", :id => "1")
       end
 
     end
