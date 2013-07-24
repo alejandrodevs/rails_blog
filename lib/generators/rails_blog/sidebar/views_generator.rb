11ac6533ea15eee54c3fc500b77e8350c1b93ecf
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-23 23:53:10 -0500

    Moving partial sidebar views into generators templates

--- lib/generators/rails_blog/sidebar/views_generator.rb
+++ lib/generators/rails_blog/sidebar/views_generator.rb
@@ -1,12 +1,10 @@
 module RailsBlog
   module Sidebar
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates/sidebar", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
 
       def create_sidebar_views
-        template "search.html.erb", "app/views/rails_blog/sidebar/_search.html.erb"
-        template "about_us.html.erb", "app/views/rails_blog/sidebar/_about_us.html.erb"
-        template "latest_posts.html.erb", "app/views/rails_blog/sidebar/_latest_posts.html.erb"
+        directory "sidebar", "app/views/rails_blog/sidebar"
       end
     end
   end
