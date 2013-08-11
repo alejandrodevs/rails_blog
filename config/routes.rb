c1d8eae5f89fe14590020d6445043b3acfecdd49
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-08-11 17:46:55 -0500

    Signed user can create posts

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ RailsBlog::Engine.routes.draw do
   match "archive" => "archives#index", via: :get, as: "archive"
   match "search"  => "searches#index", via: :get, as: "search_posts"
 
-  resources :posts, only: [:index]
+  resources :posts
   resources :users
 
   put "profile/update"
