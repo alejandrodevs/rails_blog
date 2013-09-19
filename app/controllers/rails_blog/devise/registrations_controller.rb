2d7745c187471d12f2b4a75b1cb4b9f580f53718
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-09-18 23:01:56 -0500

    Overwritting Devise update account params method

--- app/controllers/rails_blog/devise/registrations_controller.rb
+++ app/controllers/rails_blog/devise/registrations_controller.rb
@@ -2,6 +2,10 @@ module RailsBlog
   module Devise
     class RegistrationsController < ::Devise::RegistrationsController
       layout "/layouts/rails_blog/application"
+
+      def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+      end
     end
   end
 end
